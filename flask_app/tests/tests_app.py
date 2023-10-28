2731d12652b7dc0da4ed1526d5f613e4a5192e08
Nerush Kirill (home) <nerush.kirill@mail.ru>
2023-10-28 16:02:44 +0300

    ent all tests

--- flask_app/tests/tests_app.py
+++ flask_app/tests/tests_app.py
@@ -1,46 +1,83 @@
-import json
-from unittest.mock import patch
-
+import pytest
+from redis import StrictRedis
 from ..app.main import app
 
 
-def test_create_key_value():
-    with patch('app.request') as mock_request:
-        mock_request.get_json.return_value = {'key': 'test_key', 'value': 'test_value'}
-        response = app.test_client().post('/create')
-        assert response.status_code == 200
-        assert json.loads(response.data) == {"message": "Key-Value pair created successfully"}
+@pytest.fixture(scope='module')
+def test_app():
+    return app.test_client()
 
 
-def test_read_key_value():
-    with patch('app.redis_client') as mock_redis:
-        mock_redis.get.return_value = b'test_value'
-        response = app.test_client().get('/read/test_key')
-        assert response.status_code == 200
-        assert json.loads(response.data) == {'test_key': 'test_value'}
+@pytest.fixture(scope='module')
+def test_redis():
+    return StrictRedis(host='localhost', port=6379, db=0)
 
 
-def test_update_key_value():
-    with patch('app.request') as mock_request, patch('app.redis_client') as mock_redis:
-        mock_request.get_json.return_value = {'key': 'test_key', 'value': 'new_test_value'}
-        response = app.test_client().put('/update')
-        assert response.status_code == 200
-        assert json.loads(response.data) == {"message": "Key-Value pair updated successfully"}
+def test_root(test_app):
+    response = test_app.get('/')
+    assert response.status_code == 200
+    assert response.get_json() == {"message": 200}
 
 
-def test_delete_key():
-    with patch('app.redis_client') as mock_redis:
-        mock_redis.delete.return_value = 1
-        response = app.test_client().delete('/delete/test_key')
-        assert response.status_code == 200
-        assert json.loads(response.data) == {"message": "Key 'test_key' successfully deleted"}
+def test_create_key_value(test_app, test_redis):
+    payload = {
+        "key": "test_key",
+        "value": "test_value"
+    }
+    response = test_app.post('/create', json=payload)
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Key-Value pair created successfully"}
+    value = test_redis.get("test_key")
+    assert value.decode('utf-8') == "test_value"
 
 
-def test_receive_alert():
-    with patch('app.request') as mock_request:
-        mock_request.get_json.return_value = {'message': 'test_alert'}
-        with patch('builtins.print') as mock_print:
-            response = app.test_client().post('/alert')
-            assert response.status_code == 200
-            assert json.loads(response.data) == {"message": "Alert received"}
-            mock_print.assert_called_once_with("Received alert: test_alert")
+def test_read_key_value(test_app, test_redis):
+    test_redis.set("test_key", "test_value")
+    response = test_app.get('/read/test_key')
+    assert response.status_code == 200
+    assert response.get_json() == {"test_key": "test_value"}
+
+
+def test_read_key_value_not_found(test_app, test_redis):
+    response = test_app.get('/read/nonexistent_key')
+    assert response.status_code == 200
+    assert response.get_json() == {"nonexistent_key": None}
+
+
+def test_update_key_value(test_app, test_redis):
+    test_redis.set("test_key", "test_value")
+    payload = {
+        "key": "test_key",
+        "value": "updated_value"
+    }
+    response = test_app.put('/update', json=payload)
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Key-Value pair updated successfully"}
+    value = test_redis.get("test_key")
+    assert value.decode('utf-8') == "updated_value"
+
+
+def test_delete_key(test_app, test_redis):
+    test_redis.set("test_key", "test_value")
+    response = test_app.delete('/delete/test_key')
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Key 'test_key' successfully deleted"}
+    value = test_redis.get("test_key")
+    assert value is None
+
+
+def test_delete_key_not_found(test_app, test_redis):
+    response = test_app.delete('/delete/nonexistent_key')
+    assert response.status_code == 404
+    assert response.get_json() == {"message": "Key 'nonexistent_key' not found"}
+
+
+def test_receive_alert(test_app, capsys):
+    payload = {
+        "message": "Test alert"
+    }
+    response = test_app.post('/alert', json=payload)
+    assert response.status_code == 200
+    assert response.get_json() == {"message": "Alert received"}
+    captured = capsys.readouterr()
+    assert "Received alert: Test alert" in captured.out
